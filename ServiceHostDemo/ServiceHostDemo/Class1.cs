aa4056042280c63c2ed7ddeeda9e187c17166c6d
chenjianrui <chenjianrui@beisen.com>
2022-05-20 14:30:42 +0800

    add ref Beisen.Quark.QuarkClient 1.7.18

---
+++ ServiceHostDemo/ServiceHostDemo/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceHostDemo
+{
+    public class Class1
+    {
+        public static void Main()
+        {
+            // Create a ServiceHost for the CalculatorService type.
+            using (ServiceHost serviceHost =
+                   new ServiceHost(typeof(CalculatorService)))
+            {
+                // Open the ServiceHost to create listeners
+                // and start listening for messages.
+                serviceHost.Open();
+
+                // The service can now be accessed.
+                Console.WriteLine("The service is ready.");
+                Console.WriteLine("Press <ENTER> to terminate service.");
+                Console.WriteLine();
+                Console.ReadLine();
+            }
+        }
+    }
+}
